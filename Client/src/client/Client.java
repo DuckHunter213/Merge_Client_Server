15e56c5e4609b1b2ffd2844bdbc09ead6204d80e
Fercho <gomez.alejandre@gmail.com>
2017-05-03 00:09:45 -0500

    Ultimos cambios al servidor

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -3,7 +3,7 @@ package client;
 public class Client{
 
     public static void main(String[] args){
-        // TODO code application logic here
+        
     }
     
 }
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,9 +1,132 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server{
 
+    int tamanio = 100;
+    ArrayList<Integer> valores = new ArrayList<>();
+
     public static void main(String[] args){
-        // TODO code application logic here
+        Server server = new Server();
+        ArrayList<Integer> lista = server.generadorNumeros();
+        int puerto = 9090;
+
+        ServerSocket servidor = null;
+        try{
+            servidor = new ServerSocket(puerto);
+            int conexion = 0;
+            ArrayList<String> numerosOrdenadosList = new ArrayList<>();
+            while (true){ //Esperar conexiones nuevas
+                System.out.println("Esperando conexiones");
+                Socket cliente = servidor.accept();
+                //Aceptada la conexion.
+                //Creación de buffers de salida y entrada
+                BufferedReader in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                PrintWriter out = new PrintWriter(cliente.getOutputStream(), true);
+                //Recibo un mensaje para saber si me conecté
+                String mensajeConexion = in.readLine();
+                System.out.println(mensajeConexion);
+                //Envio mensaje
+                //Aquí envío el arrayList de números al cliente
+                out.println(lista);
+                //Recibo los número ordenados del cliente
+                String numerosOrdenados = in.readLine();
+                numerosOrdenadosList.add(numerosOrdenados);
+            }
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }finally{
+            try{
+                servidor.close();
+            }catch (IOException ex){
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
     }
-    
+
+    private static void run(ServerSocket servidor){
+        System.out.println("Esperando conexiones");
+        Socket sock = null;
+        try{
+            sock = servidor.accept();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally{
+            if (sock != null){
+                try{
+                    sock.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void slitList(){
+        for (Integer valore : valores){
+            System.out.print(valore.toString() + " ");
+        }
+        System.out.println("");
+        ArrayList<Integer> result = Server.merge(valores, valores);
+        for (Integer integer : result){
+            System.out.println("Integer: " + integer.toString());
+        }
+    }
+
+    public ArrayList<Integer> generadorNumeros(){
+        for (int i = 0; i < tamanio; i++){
+            Random rand = new Random();
+            valores.add(rand.nextInt(tamanio));
+        }
+        return valores;
+    }
+
+    public static <E extends Comparable<? super E>> ArrayList<Integer> merge(ArrayList<Integer> iteradorIzquierdo, ArrayList<Integer> iteradorDerecho){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Iterator<Integer> it1 = iteradorIzquierdo.iterator();
+        Iterator<Integer> it2 = iteradorDerecho.iterator();
+
+        Integer x = it1.next();
+        Integer y = it2.next();
+        while (true){
+            if (x.compareTo(y) <= 0){
+                result.add(x);
+                if (it1.hasNext()){
+                    x = it1.next();
+                }else{
+                    result.add(y);
+                    while (it2.hasNext()){
+                        result.add(it2.next());
+                    }
+                    break;
+                }
+            }else{
+                result.add(y);
+                if (it2.hasNext()){
+                    y = it2.next();
+                }else{
+                    result.add(x);
+                    while (it1.hasNext()){
+                        result.add(it1.next());
+                    }
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
 }
